98fe13f50bfe4ee30bc547e87f12648d2c0c9a00
Great Adams <kingifean@gmail.com>
2023-11-11 00:30:05 +0100

    ::

--- src/modules/v1/withdraw/withdraw.service.ts
+++ src/modules/v1/withdraw/withdraw.service.ts
@@ -50,42 +50,46 @@ class WithdrawService {
   }
 
   public async transfer(withdrawId: string) {
-    const withdraw = await this.model.findById(withdrawId)
-    if (!withdraw) throw catchError('Request not found', 404)
-    if(withdraw.status === IPaymentStatus.success) throw catchError('Withdraw request already approved', 400)
-    const user = await Promise.all([
-      this.userModel.findById(withdraw.userId),
-      this.orgModel.findById(withdraw.userId),
-    ]).catch((e) => {
-       throw catchError('User or org not found', 404);
-    });
+   try {
+      const withdraw = await this.model.findById(withdrawId)
+      if (!withdraw) throw catchError('Request not found', 404)
+      if(withdraw.status === IPaymentStatus.success) throw catchError('Withdraw request already approved', 400)
+      const user = await Promise.all([
+        this.userModel.findById(withdraw.userId),
+        this.orgModel.findById(withdraw.userId),
+      ]).catch((e) => {
+        throw catchError('User or org not found', 404);
+      });
 
-    let recipient_code
-
-
-    const createRecipiet = await transferRecipient(withdraw.account_name, withdraw.account_number, withdraw.bank_code)
-    
-    recipient_code = createRecipiet.recipient_code
+      let recipient_code
 
 
-    const data = await transferBalance(Number(withdraw.amount), recipient_code)
-    
-    const wallet = await this.walletModel.findOne({ userId: withdraw.userId })
-    const balance = wallet.balance - Number(withdraw.amount)
-    await this.walletModel.findOneAndUpdate({ userId: withdraw.userId }, { balance }, { new: true })
-    await this.walletTxModel.create({
-      amount: Number(withdraw.amount),
-      currency: 'NGN',
-      isInflow: false,
-      paymentMethod: 'WITHDRAW',
-      status: 'success',
-      userId: withdraw.userId,
-      userType: user[0] ? 'user' : 'org',
-      walletId: wallet._id
-    })
-    withdraw.status = IPaymentStatus.success
-    await withdraw.save()
-    return data
+      const createRecipiet = await transferRecipient(withdraw.account_name, withdraw.account_number, withdraw.bank_code)
+      
+      recipient_code = createRecipiet.recipient_code
+
+
+      const data = await transferBalance(Number(withdraw.amount), recipient_code)
+      
+      const wallet = await this.walletModel.findOne({ userId: withdraw.userId })
+      const balance = wallet.balance - Number(withdraw.amount)
+      await this.walletModel.findOneAndUpdate({ userId: withdraw.userId }, { balance }, { new: true })
+      await this.walletTxModel.create({
+        amount: Number(withdraw.amount),
+        currency: 'NGN',
+        isInflow: false,
+        paymentMethod: 'WITHDRAW',
+        status: 'success',
+        userId: withdraw.userId,
+        userType: user[0] ? 'user' : 'org',
+        walletId: wallet._id
+      })
+      withdraw.status = IPaymentStatus.success
+      await withdraw.save()
+      return data
+   } catch (error) {
+    console.log(error)
+   }
   }
 }
 
